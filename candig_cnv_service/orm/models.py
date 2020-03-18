38e1e4538974e88e9346b4bea243ac13bc5430c7
fcoralsasso <60624699+fcoralsasso@users.noreply.github.com>
2020-03-18 14:40:23 -0700

    Added models (#1)
    * Added models
    
    * Formated code

---
+++ candig_cnv_service/orm/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, String, Integer, ForeignKey, Float
+from sqlalchemy.orm import relationship
+
+from candig_cnv_service.orm import Base
+from candig_cnv_service.org.guid import GUID
+
+
+class Patient(Base):
+    __tablename__ = "Patient"
+
+    patient_id = Column(GUID(), primary_key=True)
+    sample_id = relationship("sample")
+
+
+class Sample(Base):
+    __tablename__ = "Sample"
+
+    sample_id = Column(String(100), primary_key=True)
+    patient_id = Column(GUID(), ForeignKey("patient.patiend_id"))
+    cnv_id = relationship("cnv")
+
+
+class CNV(Base):
+    __tablename__ = "cnv"
+    cnv_id = Column(Integer, primary_key=True)
+    sample_id = Column(String(100), ForeignKey("sample.sample_id"))
+    start_position = Column(Integer)
+    end_position = Column(Integer)
+    copy_number = Column(Float)
+    copy_number_ploidy_correcte = Column(Integer)
+    chromosome_number = Column(String(100))
