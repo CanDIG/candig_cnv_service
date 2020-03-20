d984eccde0c60ed20abd23d1e3081944e4af2a8b
fcoralsasso <60624699+fcoralsasso@users.noreply.github.com>
2020-03-20 09:24:50 -0700

    Fcoralsasso add get patient (#4)
    * Fixed table names
    
    * Added logging
    
    * Added code to getPatient method
    
    * Changed methods to snake case

--- candig_cnv_service/api/operations.py
+++ candig_cnv_service/api/operations.py
@@ -1,15 +1,45 @@
 import flask
 
+from candig_cnv_service import orm
+from candig_cnv_service.orm.models import Patient
+from candig_cnv_service.api.logging import apilog, logger
+from candig_cnv_service.api.logging import structured_log as struct_log
+
 APP = flask.current_app
 
+def _report_search_failed(typename, exception, **kwargs):
+    """
+    Generate standard log message + request error for error:
+    Internal error performing search
+    :param typename: name of type involved
+    :param exception: exception thrown by ORM
+    :param **kwargs: arbitrary keyword parameters
+    :return: Connexion Error() type to return
+    """
 
-def getPatients():
+    report = typename + " search failed"
+    message = "Internal error searching for {}s".format(typename)
+
+    logger().error(struct_log(action=report, exception=str(exception), **kwargs))
+    return dict(message=message, code=500)
+    
+
+def get_patients():
+    """
+    Return all individuals
+    """
+    try:
+        q = Patient().query.all()
+    except orm.ORMException as e:
+        err = _report_search_failed("patient", e, patient_id="all")
+        return err, 500
+
+    return [orm.dump(p) for p in q], 200
+
+
+def get_samples():
     return [], 200
 
 
-def getSamples():
-    return [], 200
-
-
-def getSegment():
+def get_segments():
     return [], 200
