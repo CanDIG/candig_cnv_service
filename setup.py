02691def7d79994b6c080183a45287ffeb121c0b
Felipe <fcoralsasso@bcgsc.ca>
2020-04-06 13:55:28 -0700

    Added requirements to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = []
+with open("requirements.txt") as requirements:
+    for line in requirements:
+        line = line.strip()
+        if len(line) == 0:
+            continue
+        if line[0] == '#':
+            continue
+
+        version_pin = line.split()[0]
+        install_requires.append(version_pin)
+
 setuptools.setup(
     name="candig-cnv-service", # Replace with your own username
     version="0.0.1",
@@ -11,6 +23,8 @@ setuptools.setup(
     description="Implementation of Copy-number variation",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    test_suite='tests',
     url="https://github.com/CanDIG/candig_cnv_service",
     packages=setuptools.find_packages(),
     classifiers=[
