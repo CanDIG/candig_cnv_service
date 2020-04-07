715a1fbe4c54546ed219bbd6703138445b5f3ff8
dashaylan <dashaylan@gmail.com>
2020-04-07 10:06:37 -0700

    Add keycloak handler

--- candig_cnv_service/api/auth/access.py
+++ candig_cnv_service/api/auth/access.py
@@ -5,6 +5,9 @@ Auth module for service
 import flask
 
 import jwt
+from jwt.algorithms import RSAAlgorithm
+from keycloak import KeycloakOpenID
+import requests
 
 from candig_cnv_service.api.logging import structured_log as struct_log
 from candig_cnv_service.api.logging import logger
@@ -32,6 +35,29 @@ def auth_key(api_key, required_scopes=None):
     # For now, any api_key to local app should work
     # TODO: refine auth methods
     return {}
+
+
+class KeyCloakHandler:
+
+    def __init__(self, config):
+        self.config = config
+        self.conn = self.connect_to_keycloak()
+        self.key_set = self.get_key_set()
+        self.public_key = RSAAlgorithm.from_jwk(self.key_set)
+
+    def connect_to_keycloak(self):
+        keycloak_openid = KeycloakOpenID(
+            server_url = self.config['KC_SERVER'] + "/auth/",
+            client_id=self.configs['OIDC_CLIENT'],
+            realm_name=self.config['KC_REALM'],
+            client_secret_key=self.config['OIDC_CLIENT_SECRET'],
+            verify=True
+        )
+        return keycloak_openid
+
+    def get_key_set(self):
+        key_response = requests.get(self.conn.well_know()['jwks_uri'])
+        print(key_response)
 
 
 def get_access_level():
