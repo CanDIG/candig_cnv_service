6ce35cbef63e9d067b879da07c34a68c74638187
Felipe <fcoralsasso@bcgsc.ca>
2020-04-23 10:58:57 -0700

    Addressed flake8 warnings

--- candig_cnv_service/__main__.py
+++ candig_cnv_service/__main__.py
@@ -64,7 +64,8 @@ def configure_app():
     app = connexion.FlaskApp(
         __name__, server="tornado", options={"swagger_url": "/"}
     )
-    api_def = pkg_resources.resource_filename("candig_cnv_service", "api/api_definition.yaml") 
+    api_def = pkg_resources.resource_filename("candig_cnv_service",
+                                              "api/api_definition.yaml")
 
     app.add_api(api_def, strict_validation=True, validate_responses=True)
 
